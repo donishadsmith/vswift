fd62ee0961d601e639af0e58477866b7f5ca043c
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2023-04-02 17:23:23 -0400

    Cleaning code

--- Categorical_CV_Split.R
+++ Categorical_CV_Split.R
@@ -57,7 +57,6 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
   x <-  cleaned_data[,-ncol(cleaned_data)]
   y <- cleaned_data[,ncol(cleaned_data)]
   
-  
   #Stratified sampling
   if(stratified == TRUE){
     #Get category names
@@ -82,13 +81,10 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
       
       test.indices <- c(test.indices,sample(indices,size = round(test.n*categories.proportions[[category]],0), replace = F))
     }
-    
-    
+
     X.train <- as.matrix(x[training.indices,]); Y.train <- as.matrix(y[training.indices])
     X.val <- as.matrix(x[test.indices,]); Y.val <- as.matrix(y[test.indices])
-    
-    
-    
+
   } else{
     #Data split
     training.indices <- sample(1:nrow(x),size = round(nrow(x)*split,0),replace = F)
@@ -105,10 +101,7 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
   }
   #Adding information to data frame
   if(!(is.null(k))){
-    
     evaluation.metrics$`K-fold CV` <- NA
-    #evaluation.metrics[nrow(evaluation.metrics):(nrow(evaluation.metrics) + (k + length(categories)*8) + 2),] <- NA
-    
     #Create folds
     rows <- sample(1:nrow(x))
     #fold size
@@ -130,12 +123,11 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
           
         }
       }
-
       #Create fold with stratified or non stratified sampling
       if(stratified == TRUE){
         # Initialize variable
         fold.idx <- c()
-        #fold.size changed to remaining rows left in the event nrow(x) is not divisible by k
+        # fold.size changed to remaining rows left in the event nrow(x) is not divisible by k
         if(i == length(1:k)){
           fold.size <- nrow(x) - (fold.size*length(1:(k - 1)))
         }
@@ -146,29 +138,25 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
           } else{
             #Stratified sampling
             fold.idx <- c(fold.idx, sample(category.list[[category]],size = round(fold.size*categories.proportions[[category]],0), replace = F))
-            
-            
           }
           #Remove already selected indices
           category.list[[category]] <- category.list[[category]][-which(category.list[[category]] %in% fold.idx)]
         }
         #Add indices to list
-        folds[[i]] <- fold.idx 
-        
-      } else{
+        folds[[i]] <- fold.idx
+        } else{
         if (i == length(1:k)) {
           #Folds may not have equal sizes
           fold.idx <- rows
-        } else {
+          } else {
           fold.idx <- rows[1:fold.size]
           #Remove rows from vectors to prevent overlapping,last fold may be smaller or larger than other folds
           rows <- rows[-c(1:fold.size)]
-        }
+          }
         #Add indices to list
-        folds[[i]] <- fold.idx 
+        folds[[i]] <- fold.idx
+        }
       }
-    }
-    
     #Add additional rownames to store corresponding values
     evaluation.metrics[nrow(evaluation.metrics):(nrow(evaluation.metrics) + length(final.metrics.names.vector) + 3),] <- NA
     #Get character vector of numbers equal to the number of rows to select rownames that need to be changed
@@ -179,7 +167,7 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
     #Assign k to iterator
     iterator <- k
     }
-  # Ensure model type is lowercase
+  #Ensure model type is lowercase
   model.type <- tolower(model.type)
   #Add it plus one to the iterator if k is not null
   iterator <- ifelse(!(is.null(k)), iterator + 1, iterator)
@@ -189,21 +177,20 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
       # Assigning data split matrices to new variable 
       X.data <- X.train
       Y.data <- Y.train
-    } else{
-      # After the first iteration the cv begins, the training set is assigned to a new variable
-      X.data <- as.matrix(x[-c(folds[[i-1]]), ]) ; Y.data <- as.matrix(y[-c(folds[[i-1]])])
-      X.val <- as.matrix(x[c(folds[[i-1]]),]) ; Y.val <- as.matrix(y[c(folds[[i-1]])])
-    }
+      } else{
+        # After the first iteration the cv begins, the training set is assigned to a new variable
+        X.data <- as.matrix(x[-c(folds[[i-1]]), ]) ; Y.data <- as.matrix(y[-c(folds[[i-1]])])
+        X.val <- as.matrix(x[c(folds[[i-1]]),]) ; Y.val <- as.matrix(y[c(folds[[i-1]])])
+        }
     #Generate model depending on chosen model.type
     switch(model.type,
            "lda" = {model <- MASS::lda(as.factor(Y.data) ~ X.data)},
            "qda" = {model <- MASS::qda(as.factor(Y.data) ~ X.data)},
            "logistic" = {model <- glm(Y.data ~ X.data, family = "binomial")}
-    )
+           )
     # Perform classification accuracy for training and test data split
     if(i == 1){
       for(j in c("Training","Test")){
-        
         if(j == "Training"){
           #Obtain prediction vector
           if(model.type != "logistic"){
@@ -212,10 +199,9 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
             if("0" %in% levels(as.factor(y))){
               prediction.vector <- prediction.vector - 1
               }
-            
-          } else{
-            prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
-            prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
+            } else{
+              prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
+              prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
             }
           
           evaluation.metrics["Classification Accuracy", j] <- sum(Y.train == prediction.vector)/length(Y.train)
@@ -229,7 +215,7 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
             evaluation.metrics[sprintf("Class %s Precision", category), j] <- true.pos/(true.pos + false.pos)
             evaluation.metrics[sprintf("Class %s Recall", category), j] <- true.pos/(true.pos + false.neg)
             }
-        } else{
+          } else{
           #Assign test set to new variable
           X.data <- X.val
           Y.data <- Y.val
@@ -239,10 +225,10 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
             #Subtract prediction vector if 0 is a level in y
             if("0" %in% levels(as.factor(y))){
               prediction.vector <- prediction.vector - 1
-            }
-          } else {
-            prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
-            prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
+              }
+            }else {
+              prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
+              prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
             }
           evaluation.metrics["Classification Accuracy", j] <- sum(Y.data == prediction.vector)/length(Y.data)
           for(category in categories){
@@ -255,105 +241,95 @@ categorical.cv.split  <- function(x = NULL, y = NULL, k = NULL, split = 0.8, mod
             evaluation.metrics[sprintf("Class %s Precision", category), j] <- true.pos/(true.pos + false.pos)
             evaluation.metrics[sprintf("Class %s Recall", category), j] <- true.pos/(true.pos + false.neg)
           }
-        }
-        }
-    } else{
-      if(!(is.null(k))){
-        if((i-1) <= k){
-          #Assign validation set to new variables
-          X.data <- X.val
-          Y.data <- Y.val
-          if(model.type != "logistic"){
-            #Obtain prediction vector
-            prediction.vector <- as.numeric(predict(model, newx = X.data)$class)
-            #Subtract prediction vector if 0 is a level in y
-            if("0" %in% levels(as.factor(y))){
-              prediction.vector <- prediction.vector - 1
-            }
-          } else{
-            prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
-            prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
           }
-          
-          #Calculate classification accuracy for fold
-          evaluation.metrics[sprintf("Fold %s Classification Accuracy",i-1), "K-fold CV"] <- sum(Y.data == prediction.vector)/nrow(Y.data)
-          
-          for(category in categories){
-            #Sum of category that was correctly guessed
-            true.pos <- sum(Y.data[which(Y.data == as.numeric(category))] == prediction.vector[which(Y.data  == as.numeric(category))])
-            #Sum of the missed category
-            false.neg <- abs(true.pos  - length(Y.data[which(Y.data  == as.numeric(category))]))
-            #Sum of the classified category
-            false.pos <- length(which(prediction.vector[-which(Y.data  == as.numeric(category))] == as.numeric(category)))
-            evaluation.metrics[sprintf("Fold %s Class %s Precision",i - 1, category), "K-fold CV"] <- true.pos/(true.pos + false.pos)
-            evaluation.metrics[sprintf("Fold %s Class %s Recall", i - 1,category), "K-fold CV"] <- true.pos/(true.pos + false.neg)
-            
-          }
-          #Calculate final metrics for CV
-          #Work in progress
-          if((i-1) == k){
-            #Add for loop to calculate these metrics'
-            #Get the relevant names
-            names.vectors <- (rownames(evaluation.metrics))[(2 + length(categories)*2):(nrow(evaluation.metrics)-length(indices))]
-            split.vector <- c()
-            for(metric in c("Classification Accuracy","Precision", "Recall")){
-              for(category in categories){
-                for(idx in 1:length(names.vectors)){
-                  # Splitting row name
-                  split.name <- unlist(strsplit(names.vectors[idx], split = " "))
-                  # Split differently depending on metric being calculated
-                  if(metric != "Classification Accuracy"){
-                    split.name <- split.name[3:length(split.name)]
-                  } 
-         
-                  
-    
-                  #If metric name and category in split.name, add to vector
-
-                  if(as.numeric(category) < 10){
-                    switch(metric,
-                           "Classification Accuracy" = {
-                             if(all("Classification" %in% split.name)){
-                               split.vector <- c(split.vector,names.vectors[idx])
-                             }
-                           },
-                           if(all(metric %in% split.name, category %in% split.name)){
-                             split.vector <- c(split.vector,names.vectors[idx])
-                           }
-                           )
-                  } else{
-                    num.vec <- unlist(strsplit(category, split = NULL))
-                    switch(metric,
-                           "Classification Accuracy" = {
-                             if(all("Classification" %in% split.name)){
-                               split.vector <- c(split.vector,names.vectors[idx])
-                             }
-                           },
-                           if(all(metric %in% split.name, num.vec[1] %in% split.name & num.vec[2] %in% split.name)){
-                             split.vector <- c(split.vector,names.vectors[idx])
-                           })
+        }
+      } else{
+          if(!(is.null(k))){
+            if((i-1) <= k){
+              #Assign validation set to new variables
+              X.data <- X.val
+              Y.data <- Y.val
+              if(model.type != "logistic"){
+                #Obtain prediction vector
+                prediction.vector <- as.numeric(predict(model, newx = X.data)$class)
+                #Subtract prediction vector if 0 is a level in y
+                if("0" %in% levels(as.factor(y))){
+                  prediction.vector <- prediction.vector - 1
                   }
-                  
-                  if(idx == length(names.vectors)){
-                    if(metric == "Classification Accuracy"){
-                      evaluation.metrics[sprintf("Mean CV %s", metric), "K-fold CV"] <- mean(evaluation.metrics[split.vector, "K-fold CV"])
-                      evaluation.metrics[sprintf("SD CV %s", metric), "K-fold CV"] <- sd(evaluation.metrics[split.vector, "K-fold CV"])
-                    } else{
-                      evaluation.metrics[sprintf("Mean CV Class %s %s", category, metric), "K-fold CV"] <- mean(evaluation.metrics[split.vector, "K-fold CV"])
-                      evaluation.metrics[sprintf("SD CV Class %s %s", category, metric), "K-fold CV"] <- sd(evaluation.metrics[split.vector, "K-fold CV"])
+                }else{
+                  prediction.vector <- predict(model, newdata = data.frame(X.data), type = "response")
+                  prediction.vector <- ifelse(prediction.vector > 0.5, 1, 0)
+                }
+              
+              #Calculate classification accuracy for fold
+              evaluation.metrics[sprintf("Fold %s Classification Accuracy",i-1), "K-fold CV"] <- sum(Y.data == prediction.vector)/nrow(Y.data)
+              for(category in categories){
+                #Sum of category that was correctly guessed
+                true.pos <- sum(Y.data[which(Y.data == as.numeric(category))] == prediction.vector[which(Y.data  == as.numeric(category))])
+                #Sum of the missed category
+                false.neg <- abs(true.pos  - length(Y.data[which(Y.data  == as.numeric(category))]))
+                #Sum of the classified category
+                false.pos <- length(which(prediction.vector[-which(Y.data  == as.numeric(category))] == as.numeric(category)))
+                evaluation.metrics[sprintf("Fold %s Class %s Precision",i - 1, category), "K-fold CV"] <- true.pos/(true.pos + false.pos)
+                evaluation.metrics[sprintf("Fold %s Class %s Recall", i - 1,category), "K-fold CV"] <- true.pos/(true.pos + false.neg)
+                }
+              #Calculate final metrics for CV
+              #Work in progress
+              if((i-1) == k){
+                #Add for loop to calculate these metrics'
+                #Get the relevant names
+                names.vectors <- (rownames(evaluation.metrics))[(2 + length(categories)*2):(nrow(evaluation.metrics)-length(indices))]
+                split.vector <- c()
+                for(metric in c("Classification Accuracy","Precision", "Recall")){
+                  for(category in categories){
+                    for(idx in 1:length(names.vectors)){
+                      # Splitting row name
+                      split.name <- unlist(strsplit(names.vectors[idx], split = " "))
+                      # Split differently depending on metric being calculated
+                      if(metric != "Classification Accuracy"){
+                        split.name <- split.name[3:length(split.name)]
+                        } 
+                      #If metric name and category in split.name, add to vector
+                      if(as.numeric(category) < 10){
+                        switch(metric,
+                               "Classification Accuracy" = {
+                                 if(all("Classification" %in% split.name)){
+                                   split.vector <- c(split.vector,names.vectors[idx])
+                                   }},
+                               if(all(metric %in% split.name, category %in% split.name)){
+                                 split.vector <- c(split.vector,names.vectors[idx])
+                               })
+                        }else{
+                          num.vec <- unlist(strsplit(category, split = NULL))
+                          switch(metric,
+                                 "Classification Accuracy" = {
+                                   if(all("Classification" %in% split.name)){
+                                     split.vector <- c(split.vector,names.vectors[idx])
+                                   }},
+                                 if(all(metric %in% split.name, num.vec[1] %in% split.name & num.vec[2] %in% split.name)){
+                                   split.vector <- c(split.vector,names.vectors[idx])
+                                 })
+                          }
+                      if(idx == length(names.vectors)){
+                        if(metric == "Classification Accuracy"){
+                          evaluation.metrics[sprintf("Mean CV %s", metric), "K-fold CV"] <- mean(evaluation.metrics[split.vector, "K-fold CV"])
+                          evaluation.metrics[sprintf("SD CV %s", metric), "K-fold CV"] <- sd(evaluation.metrics[split.vector, "K-fold CV"])
+                          }else{
+                            evaluation.metrics[sprintf("Mean CV Class %s %s", category, metric), "K-fold CV"] <- mean(evaluation.metrics[split.vector, "K-fold CV"])
+                            evaluation.metrics[sprintf("SD CV Class %s %s", category, metric), "K-fold CV"] <- sd(evaluation.metrics[split.vector, "K-fold CV"])
+                            }
+                        #Empty vector
+                        split.vector <- c()
+                      }
                     }
-                    
-                    #Empty vector
-                    split.vector <- c()
                   }
                 }
               }
             }
           }
-        }
       }
-    }
     }
   return(evaluation.metrics)
   }
+
 
