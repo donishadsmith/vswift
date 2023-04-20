0f1f0aa5174a583ec75d0217caa7031ecb47c60e
donishadsmith <donishasmith@outlook.com>
2023-04-20 00:28:36 -0400

    fixes for stratified_check

--- R/.stratified_check.R
+++ R/.stratified_check.R
@@ -1,8 +1,8 @@
 .stratified_check <- function(class,class_indices,output,n){
-  if(round(n*output[["class_proportions"]][[class]],0)){
+  if(round(n*output[["class_proportions"]][[class]],0) == 0){
     stop(sprintf("0 indices selected for %s class\n not enough samples for stratified sampling",class))
   }
-  if(round(n*output[["class_proportions"]][[class]],0) > class_indices[[class]]){
+  if(round(n*output[["class_proportions"]][[class]],0) > length(class_indices[[class]])){
     stop(sprintf("not enough samples of %s class for stratified sampling",class))
   }
 }
