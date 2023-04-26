cc340cecb25c4b6e80d361918e483ccb08273a5f
donishadsmith <donishasmith@outlook.com>
2023-04-26 19:10:34 -0400

    add documentation

--- pkg/vswift/package-doc.R
+++ pkg/vswift/package-doc.R
@@ -2,10 +2,11 @@
 #'
 #' A package for simple classification model evaluation.
 #'
-#' @name vswift
+#' @aliases vswift
 #' @docType package
 #' @title vswift
 #' @description A package that allows for model evaluation using train-test splitting and k-fold cross validation using several popular classification
 #'  algorithms such as logistic regression, linear discriminant analysis, quadratic discriminant analysis, support vector machines, k-nearest neighbors, 
 #'  artificial neural network, decision trees, and random forest.
 #' @author Donisha Smith
+NULL
