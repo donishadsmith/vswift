88497ed723305bb6ac3f6cc85c7d9d83f9679ed0
donishadsmith <donishasmith@outlook.com>
2023-05-10 19:42:50 -0400

    Revert "seperate internal functions into seperate files"
    This reverts commit 4f11da822282bea973dc706cc3e970d4e5c0fd5a.

--- pkg/vswift/R/utils.R
+++ pkg/vswift/R/utils.R
@@ -1,3 +1,105 @@
+#Helper function for classCV and genFolds to check if inputs are valid
+.error_handling <- function(data = NULL, target = NULL, predictors = NULL, split = NULL, n_folds = NULL, model_type = NULL, threshold = NULL, stratified = NULL,  random_seed = NULL,
+                            impute_method = NULL, impute_args = NULL, call = NULL,...){
+  #Valid models
+  valid_models <- c("lda","qda","logistic","svm","naivebayes","ann","knn","decisiontree",
+                    "randomforest", "multinom", "gbm")
+  valid_impute <- c("simple", "missforest")
+  # Ensure data is not NULL
+  if(is.null(data)){
+    stop("no input data")
+  }
+  
+  # Check if impute method is valid
+  if(!is.null(impute_method)){
+    if(!impute_method %in% valid_impute){
+      stop("invalid impute method")
+    }
+    # Check if impute method is valid
+    if(!is.null(impute_args)){
+      if(all(impute_method == "missforest", class(impute_args) != "list")){
+        stop("impute_args must be a list")
+      }
+    }
+  }
+  # Check if additional arguments are valid
+  if(all(impute_method == "missforest",!is.null(impute_args))){
+    vswift:::.check_additional_arguments(impute_method = impute_method, impute_args = impute_args)
+  }
+  if(length(list(...)) > 0){
+    vswift:::.check_additional_arguments(model_type = model_type, ...)
+  }
+  # Ensure n_folds is not an invalid number
+  if(!is.data.frame(data)){
+    stop("invalid input for data")
+  }
+  
+  # Ensure fold size is valid
+  if(any(n_folds %in% c(0,1), n_folds < 0, n_folds > 30,is.character(n_folds), n_folds != as.integer(n_folds))){
+    stop("`n_folds` must be a non-negative integer from 3-30")
+  }
+  
+  # Ensure split is between 0.5 to 0.9
+  if(any(is.character(split), split < 0.5, split > 0.9)){
+    stop("split must be a numeric value from between 0.5 and 0.9")
+  }
+  
+  if(call == "classCV" || call == "genFolds" & stratified == TRUE){
+    # Ensure target is not null
+    if(is.null(target)){
+      stop("target has no input")
+    }
+    # Ensure target is also not in predictors 
+    if(target %in% predictors){
+      stop("target cannot also be a predictor")
+    }
+    
+    # Ensure there is only one target variable
+    if(length(target) != 1){
+      stop("length of target must be 1")
+    }
+    
+    # Check if target is in dataframe
+    if(is.numeric(target)){
+      if(!(target %in% c(1:ncol(data)))){
+        stop("target not in dataframe")
+      }
+    }else if(is.character(target)){
+      if(!(target %in% colnames(data))){
+        stop("target not in dataframe")
+      }
+    } else {
+      stop("target must be an integer or character")
+    }
+  }
+  # Check if predictors are in data frame
+  if(!is.null(predictors)){
+    if(all(is.numeric(predictors))){
+      check_x <- 1:dim(data)[1]
+    }else if(all(is.character(predictors))){
+      check_x <- colnames(data)[colnames(data) != target]
+    } else {
+      stop("predictors must be a character vector or integer vector")
+    }
+    if(!(all(predictors %in% check_x))){
+      stop("at least one predictor is not in dataframe")
+    }
+  }
+  #Ensure model_type has been assigned
+  if(call == "classCV"){
+    if(any(is.null(model_type), !(model_type %in% valid_models))){
+      stop(sprintf("%s is an invalid model_type", model_type))
+    }
+    if(model_type == "logistic" & any(length(levels(factor(data[,target], exclude = NA))) != 2, !is.numeric(threshold), threshold < 0.30 || threshold > 0.70)){
+      if(length(levels(factor(data[,target], exclude = NA))) != 2){
+        stop("logistic regression requires a binary variable")
+      } else {
+        stop("threshold must a numeric value from 0.30 to 0.70")
+      }
+    }
+  }
+}
+
 # Helper function for imputation
 
 .impute <- function(data = NULL, missing_columns = NULL, impute_method = NULL, impute_args = NULL){
@@ -233,6 +335,185 @@
   # Return list
   return(calculate_metrics_list)
 }
+# Helper function to perform proper plotting depending on where command is ran
+.check_env <- function(){
+  system = as.character(Sys.info()["sysname"])
+  if(Sys.getenv("RStudio") == "1"){
+    new_window  <- ifelse(rstudioapi::isAvailable(), function(){placeholder = "placeholder"},ifelse(system == "Windows", windows, x11))
+  } else {
+    new_window <- ifelse(system == "Windows", windows, x11)
+  }
+  return(new_window)
+}
+# Helper function for regular plotting
+.visible_plots <- function(object, split, cv){
+  # Check if RStudio or GUI is running for proper plotting
+  new_window <- vswift:::.check_env()
+  if(all(is.data.frame(object[["metrics"]][["split"]]), split == TRUE)){
+    # Plot metrics for training and test
+    new_window()
+    plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,"Classification Accuracy"] , ylim = c(0,1), xlab = "Set", ylab = "Classification Accuracy", xaxt = "n")
+    axis(1, at = 1:2, labels = c("Training","Test"))
+    # Iterate over classes
+    for(class in as.character(object[["classes"]][[1]])){
+      new_window()
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s Precision", class)] , ylim = c(0,1), xlab = "Set", ylab = "Precision" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+      
+      new_window()
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s Recall", class)] , ylim = c(0,1), xlab = "Set", ylab = "Recall" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+      
+      new_window()
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s F-Score", class)] , ylim = c(0,1), xlab = "Set", ylab = "F-Score" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+    }
+  }
+  # Plot metrics for training and test
+  if(all(is.data.frame(object[["metrics"]][["cv"]]), cv == TRUE)){
+    # To get the correct class for plot title
+    class_idx <- 1
+    # Get the last row index subtracted by three to avoid getting mean, standard dev, and standard error
+    idx <- nrow(object[["metrics"]][["cv"]]) - 3
+    fold_n <- idx
+    # Initialize new metrics
+    for(colname in colnames(object[["metrics"]][["cv"]])[colnames(object[["metrics"]][["cv"]]) != "Fold"]){
+      num_vector <- object[["metrics"]][["cv"]][1:idx, colname]
+      # Split column name
+      split_vector <- unlist(strsplit(colname, split = " "))
+      # Depending on column name, plotting is handled slightly differently
+      if("Classification" %in% split_vector){
+        new_window()
+        plot(x = 1:fold_n, y = num_vector, ylim = c(0,1), xlab = "K-folds", ylab = "Classification Accuracy" , xaxt = "n")
+        axis(side = 1, at = as.integer(1:fold_n), labels = as.integer(1:fold_n))
+      } else {
+        # Get correct metric name for plot y title
+        y_name <- c("Precision","Recall","F-Score")[which(c("Precision","Recall","F-Score") %in% split_vector)]
+        new_window()
+        plot(x = 1:fold_n, y = num_vector, ylim = c(0,1), xlab = "K-folds", ylab = y_name, main = paste("Class: ",as.character(object[["classes"]][[1]])[[class_idx]]), xaxt = "n") 
+        axis(side = 1, at = as.integer(1:fold_n), labels = as.integer(1:fold_n))
+        # Add 1 to `class_idx` when `y_name == "Recall"` to get correct class plot title
+        if(y_name == "F-Score"){
+          class_idx <- class_idx + 1
+        }
+      }
+      # Add mean and standard deviation to the plot
+      abline(h = mean(num_vector), col = "red", lwd = 1)
+      abline(h = mean(num_vector) + sd(num_vector), col = "blue", lty = 2, lwd = 1)
+      abline(h = mean(num_vector) - sd(num_vector), col = "blue", lty = 2, lwd = 1)
+    }
+  }
+}
+ 
+.dev_off_and_new <- function(){
+  # Don't display plot if save_plot is TRUE
+  if(all(Sys.getenv("RStudio") == "1",rstudioapi::isAvailable())){
+    graphics.off()
+  } else {
+    dev.off()
+  }
+}
+
+.save_plots <- function(object, path, split, cv, ...){
+  if(all(is.data.frame(object[["metrics"]][["split"]]), split == TRUE)){
+    # Save metrics for training and test
+    png(filename = paste0(path,"train_test_classification_accuracy.png"),...)
+    plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,"Classification Accuracy"] , ylim = c(0,1), xlab = "Set", ylab = "Classification Accuracy", xaxt = "n")
+    axis(1, at = 1:2, labels = c("Training","Test"))
+    vswift:::.dev_off_and_new()
+    # Iterate over classes
+    for(class in as.character(object[["classes"]][[1]])){
+      # Save metrics for training and test
+      png(filename = paste0(path,sprintf("train_test_precision_%s.png",class)),...)
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s Precision", class)] , ylim = c(0,1), xlab = "Set", ylab = "Precision" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+      # Don't display plot and create new plot
+      vswift:::.dev_off_and_new()
+      # Save metrics for training and test
+      png(filename = paste0(path,sprintf("train_test_recall_%s.png",class)),...)
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s Recall", class)] , ylim = c(0,1), xlab = "Set", ylab = "Recall" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+      # Don't display plot and create new plot
+      vswift:::.dev_off_and_new()
+      # Save metrics for training and test
+      png(filename = paste0(path,sprintf("train_test_f-score_%s.png",  class)),...)
+      plot(x = 1:2, y = object[["metrics"]][["split"]][1:2,sprintf("Class: %s F-Score", class)] , ylim = c(0,1), xlab = "Set", ylab = "F-Score" , xaxt = "n",
+           main = paste("Class:",class))
+      axis(1, at = 1:2, labels = c("Training","Test"))
+    }
+  }
+  # Plot metrics for training and test
+  if(all(is.data.frame(object[["metrics"]][["cv"]]), cv == TRUE)){
+    # To get the correct class for plot title
+    class_idx <- 1
+    # Get the last row index subtracted by three to avoid getting mean, standard dev, and standard error
+    idx <- nrow(object[["metrics"]][["cv"]]) - 3
+    fold_n <- idx
+    # Initialize new metrics
+    for(colname in colnames(object[["metrics"]][["cv"]])[colnames(object[["metrics"]][["cv"]]) != "Fold"]){
+      num_vector <- object[["metrics"]][["cv"]][1:idx, colname]
+      # Split column name
+      split_vector <- unlist(strsplit(colname, split = " "))
+      # Depending on column name, plotting is handled slightly differently
+      if("Classification" %in% split_vector){
+        # Save metrics for cv
+        png(filename = paste0(path,"cv_classification_accuracy.png"),...)
+        plot(x = 1:fold_n, y = num_vector, ylim = c(0,1), xlab = "K-folds", ylab = "Classification Accuracy" , xaxt = "n")
+        axis(side = 1, at = as.integer(1:fold_n), labels = as.integer(1:fold_n))
+      } else {
+        # Get correct metric name for plot y title
+        y_name <- c("Precision","Recall","F-Score")[which(c("Precision","Recall","F-Score") %in% split_vector)]
+        # Save metrics for cv
+        png(filename = paste0(path,sprintf("cv_%s_%s.png",tolower(y_name),as.character(object[["classes"]][[1]])[[class_idx]])),...)
+        file <- paste0(path,sprintf("cv_%s_%s.png",tolower(y_name),as.character(object[["classes"]][[1]])[[class_idx]]))
+        plot(x = 1:fold_n, y = num_vector, ylim = c(0,1), xlab = "K-folds", ylab = y_name, main = paste("Class: ",as.character(object[["classes"]][[1]])[[class_idx]]), xaxt = "n") 
+        axis(side = 1, at = as.integer(1:fold_n), labels = as.integer(1:fold_n))
+        # Add 1 to `class_idx` when `y_name == "Recall"` to get correct class plot title
+        if(y_name == "F-Score"){
+          class_idx <- class_idx + 1
+        }
+      }
+      # Add mean and standard deviation to the plot
+      abline(h = mean(num_vector), col = "red", lwd = 1)
+      abline(h = mean(num_vector) + sd(num_vector), col = "blue", lty = 2, lwd = 1)
+      abline(h = mean(num_vector) - sd(num_vector), col = "blue", lty = 2, lwd = 1)
+      # Don't display plot and create new plot
+      vswift:::.dev_off_and_new()
+    }
+  }
+}
+
+# Helper function for classCV to create model
+.generate_model <- function(model_type = NULL, formula = NULL, predictors = NULL, target = NULL, model_data = NULL, ...){
+  switch(model_type,
+         # Use double colon to avoid cluttering user space
+         "lda" = {model <- MASS::lda(formula, data = model_data, ...)},
+         "qda" = {model <- MASS::qda(formula, data = model_data, ...)},
+         "logistic" = {model <- glm(formula, data = model_data , family = "binomial", ...)},
+         "svm" = {model <- e1071::svm(formula, data = model_data, ...)},
+         "naivebayes" = {model <- naivebayes::naive_bayes(formula = formula, data = model_data, ...)},
+         "ann" = {model <- nnet::nnet(formula = formula, data = model_data, ...)},
+         "knn" = {model <- kknn::train.kknn(formula = formula, data = model_data, ...)},
+         "decisiontree" = {model <- rpart::rpart(formula = formula, data = model_data, ...)},
+         "randomforest" = {model <- randomForest::randomForest(formula = formula, data = model_data, ...)},
+         "multinom" = {model <- nnet::multinom(formula = formula, data = model_data, ...)},
+         "gbm" = {
+           model_data <- as.matrix(model_data)
+           if(!is.null(predictors)){
+             xgb_data <- xgboost::xgb.DMatrix(data = model_data[,predictors], label = model_data[,target])
+           } else {
+             xgb_data <- xgboost::xgb.DMatrix(data = model_data[,colnames(model_data)[colnames(model_data) != target]], label = model_data[,target])
+           }
+           model <- xgboost::xgb.train(data = xgb_data, ...)}
+  )
+  return(model)
+}
+
 
 # Helper function for classCV to predict 
 
