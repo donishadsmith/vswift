2be0991b8656da0a89a7eda01b81f89071cef35a
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2024-09-12 17:52:12 -0400

    add test

--- pkg/vswift/R/split.R
+++ pkg/vswift/R/split.R
@@ -1,9 +1,9 @@
 # Function to perform train-test split with no stratification
 #' @noRd
 #' @export
-.split <- function(N, split, random_seed) {
+.split <- function(N, split, random_seed){
   # Set seed
-  if(!is.null(random_seed)) set.seed(random_seed)
+  if (!is.null(random_seed)) set.seed(random_seed)
   
   split_indxs <- list("train" = NULL, "test" = NULL)
   # Create test and train set
@@ -20,7 +20,7 @@
 #' @export 
 .cv <- function(N, n_folds, random_seed){
   # Set seed
-  if(!is.null(random_seed)) set.seed(random_seed)
+  if (!is.null(random_seed)) set.seed(random_seed)
   
   # Initialize list
   cv_indxs <- list()
@@ -31,14 +31,14 @@
   excess <- N - sum(folds_vec)
   if (excess > 0) {
     folds_vector <- rep(1:n_folds, excess)[1:excess]
-    for(num in folds_vector) folds_vec[num] <- folds_vec[num] + 1
+    for (num in folds_vector) folds_vec[num] <- folds_vec[num] + 1
   }
   # Random shuffle
   folds_vec <- sample(folds_vec, size = length(folds_vec), replace = FALSE)
-  for(i in 1:n_folds){
+  for (i in 1:n_folds) {
     # Create fold with stratified or non stratified sampling
     fold_indxs <- indices[1:folds_vec[i]]
-    # Remove rows from vectors to prevent overlapping,l ast fold may be smaller or larger than other folds
+    # Remove rows from vectors to prevent overlapping, last fold may be smaller or larger than other folds
     indices <- indices[-c(1:folds_vec[i])]
     # Add indices to list
     cv_indxs[[sprintf("fold%s",i)]] <- fold_indxs
