10b88ebfa457416f154c62523cda0f16e5f0ac32
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2025-05-25 23:34:56 -0400

    Re-export kknn function to namespace (#14)

--- R/contr.dummy.R
+++ R/contr.dummy.R
@@ -2,10 +2,22 @@
 #'
 #' This function is re-exported from the \code{kknn} package to ensure compatibility with the \code{train.kknn} function,
 #' which requires \code{contr.dummy} to be available in the current namespace. This function generates dummy variables
-#' for factors
+#' for unordered factors.
 #'
 #' @name contr.dummy
 #' @keywords internal
 #' @export
 #' @importFrom kknn contr.dummy
+NULL
+
+#' Internal Re-export of contr.ordinal
+#'
+#' This function is re-exported from the \code{kknn} package to ensure compatibility with the \code{train.kknn} function,
+#' which requires \code{contr.ordinal} to be available in the current namespace. This function generates dummy variables
+#' for ordered factors.
+#'
+#' @name contr.ordinal
+#' @keywords internal
+#' @export
+#' @importFrom kknn contr.ordinal
 NULL
